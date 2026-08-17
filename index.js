35a70e52eac46f182efd8d41fa56134ff36df519
Greg Stoll <greg@gregstoll.com>
2026-08-17 17:12:31 -0500

    set up TargetColor

--- index.js
+++ index.js
@@ -4,10 +4,112 @@ import colorlab from "https://cdn.jsdelivr.net/npm/colorlab@0.2/+esm";
 const { CIELAB, CIEDE2000 } = colorlab.default ?? colorlab;
 import {lab} from "https://cdn.jsdelivr.net/npm/d3-color@3/+esm";
 
-let x = lab("#f70022");
+/*let x = lab("#f70022");
 let y = lab("#f7022a");
 
 const lab1 = new CIELAB(x.l, x.a, x.b);
 const lab2 = new CIELAB(y.l, y.a, y.b);
-console.log(CIEDE2000(lab1, lab2));
+console.log(CIEDE2000(lab1, lab2));*/
 
+const ColorRegex = new RegExp("^[0-9a-fA-F]{6}$");
+/**
+ * 
+ * @param {string} text the color to test
+ * @returns whether it's of the form "#aabbcc"
+ */
+export function colorIsValid(text) {
+  if (!text.startsWith("#")) {
+    return false;
+  }
+  if (text.length !== 7) {
+    return false;
+  }
+  return ColorRegex.test(text.substring(1));
+}
+
+class TargetColor extends HTMLElement {
+    /** @type string */
+    #color;
+    /** @type string */
+    #lastValidColor;
+
+    static get observedAttributes() {
+        return ['color'];
+    }
+    attributeChangedCallback(_name, _oldValue, _newValue) {
+        this.update();
+    }
+
+    connectedCallback() {
+         if (this.querySelector('form')) return;
+         this.innerHTML = `
+            <form>
+                <label htmlFor="colorText">Color to name: </label>
+                <input type="text" name="colorText"></input>
+                &nbsp;<input type="color"></input>
+            </form>`;
+        this.textElement.addEventListener("change", this.handleColorChange.bind(this));
+        this.colorElement.addEventListener("change", this.handleColorChange.bind(this));
+        let textElement = this.querySelector('input[type="text"]');
+
+        this.update();
+    }
+
+    get textElement() {
+        return this.querySelector('input[type="text"]');
+    }
+    get colorElement() {
+        return this.querySelector('input[type="color"]');
+    }
+
+    update() {
+        if (!this.querySelector('form')) return;
+        let newColor = this.getAttribute("color");
+        // TODO - handle lastValidColor
+        // TODO - show color (lastValidColor?) in text
+        if (this.#color !== newColor) {
+            this.#color = newColor;
+            this.#lastValidColor = newColor;
+            this.textElement.value = this.#color;
+            this.colorElement.value = this.#color;
+        }
+    }
+
+    handleColorChange(event) {
+        let text = event.target.value.trim();
+        if (!text.startsWith('#')) {
+            text = '#' + text;
+        }
+        this.dispatchEvent(new CustomEvent("colorChange", {
+            detail: { color: text, colorIsValid: colorIsValid(text) }
+        }));
+    }
+}
+customElements.define('target-color', TargetColor);
+
+class ColorSet extends HTMLElement {
+
+}
+customElements.define('color-set', ColorSet);
+
+class NameMyColorApp extends HTMLElement {
+    #color = "#f70022";
+    connectedCallback() {
+        if (this.querySelector("target-color")) return;
+        this.innerHTML = `<target-color></target-color>`;
+        this.targetColor.addEventListener("colorChange", e => {
+            if (e.detail.colorIsValid) {
+                this.#color = e.detail.color;
+                this.update();
+            }
+        });
+        this.update();
+    }
+    get targetColor() {
+        return this.querySelector("target-color");
+    }
+    update() {
+        this.targetColor.setAttribute("color", this.#color);
+    }
+}
+customElements.define('name-my-color-app', NameMyColorApp);
